0d7be6866cc0e34b0761abdd8432a37915b45b53
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-04-16 16:34:10 -0700

    git fix

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-
+const apiKey = 'AIzaSyDxggzPT57iqEo-tohF6HqdpYj4YmIopnM';
 const authService = {
     signIn: async function signInService(user) {
 
-        const fbuser = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxggzPT57iqEo-tohF6HqdpYj4YmIopnM', { ...user, returnSecureToken: true });
+        const fbuser = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${apiKey}`, { ...user, returnSecureToken: true });
         return fbuser;
     },
     signUp: async function signUpService(user) {
 
-        const fbuser = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDxggzPT57iqEo-tohF6HqdpYj4YmIopnM', { ...user, returnSecureToken: true });
+        const fbuser = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, { ...user, returnSecureToken: true });
         return fbuser;
     }
 
