96d38afbb29981bf797efacce3b77b20885dce58
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-04-22 11:27:50 -0700

    revised definition

--- src/features/RecordsManager/RecordsManager.js
+++ src/features/RecordsManager/RecordsManager.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from 'react';
 import CoreForm from './components/CoreForm';
 import CoreList from './components/CoreList';
 import firebaseService from '../../services/firebaseService';
-import _ from 'lodash';
-
 
 const RecordsManager = ({ fbUser, definition }) => {
     const [importMessage, setImportMessage] = useState('');
     const [importUrl, setImportUrl] = useState('');
     const [records, setRecords] = useState([]);
+    definition = normalizeDefinition(definition);
     const initialRecords = {};
     definition.fields.forEach(element => {
         initialRecords[element.name] = element.defaultValue;
@@ -22,8 +21,7 @@ const RecordsManager = ({ fbUser, definition }) => {
                 const result = await svc.getRecords(fbUser);
                 if (result && result.data) {
                     const objectKeys = Object.keys(result.data);
-                    const keyField = definition.fields.find(e => e.isKey);
-                    const mappedRecords = objectKeys.map(k => { return { ...result.data[k], [keyField.name]: k } });
+                    const mappedRecords = objectKeys.map(k => { return { ...result.data[k], firebaseId: k} });
                     setRecords(mappedRecords);
                     setMode(0);
                 } else {
@@ -39,12 +37,12 @@ const RecordsManager = ({ fbUser, definition }) => {
         setMode(0)
     };
     const handleOnUpdated = (updatedRecord) => {
-        const updatedRecords = records.map(c => c.id === updatedRecord.id ? updatedRecord : c);
+        const updatedRecords = records.map(c => c.firebaseId === updatedRecord.firebaseId ? updatedRecord : c);
         setRecords(updatedRecords);
         setMode(0)
     };
     const handleOnDeleted = (deletedRecord) => {
-        const remainingRecords = records.filter(x => x.id !== deletedRecord.id);
+        const remainingRecords = records.filter(x => x.firebaseId !== deletedRecord.firebaseId);
         setRecords(remainingRecords);
         setMode(0)
     };
@@ -72,8 +70,6 @@ const RecordsManager = ({ fbUser, definition }) => {
             }
             const importRecords = await (await svc.importFrom(importUrl)).data;
             setImportMessage(`Found ${importRecords.length} ${definition.name} records. Importing...`);
-            const keyField = _.first(definition.fields, { 'isKey': true });
-
             importRecords.forEach(r => {
                 setTimeout(() => {
                     const newImport = {};
@@ -83,7 +79,7 @@ const RecordsManager = ({ fbUser, definition }) => {
                         newImport[normalizedKey] = r[ik]
                     })
                     svc.createRecord(fbUser, newImport);
-                    setImportMessage(`Importing ${definition.name} record# ${newImport[keyField.name]}`)
+                    setImportMessage(`Importing ${definition.name} record# ${newImport[definition.fields[0].name]}`)
                     setRecords([...records, newImport]);
                 }, 2000)
             });
@@ -103,6 +99,33 @@ const RecordsManager = ({ fbUser, definition }) => {
 
         </React.Fragment >
     )
+}
+
+function normalizeDefinition(definition){
+    definition.name = definition.name[0].toUpperCase() + definition.name.toLowerCase().substring(1);
+    definition.fields = definition.fields.map(f=> {
+        const label = (f.label || (f.name[0].toUpperCase() + f.name.substring(1)));
+        const type = (f.type || 'text');
+        const summary = f.summary || 0;
+        const defaultValue = f.defaultValue || '';
+        const readonly = f.readonly || false;
+        return {
+            ...f,
+            label,
+            type,
+            summary,
+            readonly,
+            defaultValue,
+        };
+    });
+
+    if (!definition.fields.some(s=> s.summary)){
+        definition.fields[0].summary = 1;
+    }
+    if (!definition.fields.some(s=> s.autofocus)){
+        definition.fields[0].autofocus = true;
+    }
+    return definition;
 }
 
 export default RecordsManager;
