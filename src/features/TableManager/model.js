f6ed86cef66c9cb956530f5b3e9f68295687e310
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-05-12 18:06:52 -0700

    add child

--- src/features/TableManager/model.js
+++ src/features/TableManager/model.js
@@ -3,9 +3,9 @@ const model = {
   child: 'column',
   fields: [
     { name: 'name', label: 'Table name' },
+    { name: 'child', label: 'Child name' },
     {name: 'enabled', label: 'Active', type: 'checkbox', defaultValue: true}
   ]
 }
-
 
 export default model;
